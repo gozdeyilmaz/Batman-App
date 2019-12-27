0220bb5070d58cfd6995d49b835db3371c797cbc
gozdeylmz95 <gozdeylmz95@gmail.com>
2019-12-27 18:05:26 +0300

    Updates

---
+++ precache-manifest.2470e2dbd7c9aaa9d60693c6dc1ea2c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a9700525d1ff6ac11e8b6e177b1ac05",
+    "url": "/Batman-App/index.html"
+  },
+  {
+    "revision": "5b00e0274f2ad4c0fdbb",
+    "url": "/Batman-App/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "0642400c15e5b4df7fb8",
+    "url": "/Batman-App/static/css/main.84739e53.chunk.css"
+  },
+  {
+    "revision": "5b00e0274f2ad4c0fdbb",
+    "url": "/Batman-App/static/js/2.1ddf4fa4.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/Batman-App/static/js/2.1ddf4fa4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0642400c15e5b4df7fb8",
+    "url": "/Batman-App/static/js/main.80003d16.chunk.js"
+  },
+  {
+    "revision": "cf2d32de86bd09858de8",
+    "url": "/Batman-App/static/js/runtime-main.d57a9546.js"
+  }
+]);
